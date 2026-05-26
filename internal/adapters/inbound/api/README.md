0f34b44b3bc5e852f298e6a37ce234923d7718ae
dctalbot <dave.c.talbot@gmail.com>
2026-05-25 23:15:25 -0400

    docs

---
+++ internal/adapters/inbound/api/README.md
@@ -0,0 +1,26 @@
+## Example HTTP responses
+
+// 2XX
+// ok
+// created
+// accepted
+// no_content
+
+// 4XX
+// bad_request
+// unauthorized
+// forbidden
+// not_found
+// method_not_allowed
+// not_acceptable
+// request_timeout
+// conflict
+// unprocessable_entity
+// too_many_requests
+
+// 5XX
+// internal_server_error
+// not_implemented
+// bad_gateway
+// service_unavailable
+// gateway_timeout
--- internal/apperrors/errors.go
+++ internal/apperrors/errors.go
@@ -8,3 +8,30 @@ var (
 	ErrConflict  = errors.New("conflict")
 	ErrForbidden = errors.New("forbidden")
 )
+
+// TODO
+// conventional validation error messages:
+// accepted
+// blank
+// present
+// confirmation
+// empty
+// equal_to
+// even
+// exclusion
+// greater_than
+// greater_than_or_equal_to
+// inclusion
+// invalid
+// less_than
+// less_than_or_equal_to
+// model_invalid
+// not_a_number
+// not_an_integer
+// odd
+// other_than
+// required
+// taken
+// too_long
+// too_short
+// wrong_length
